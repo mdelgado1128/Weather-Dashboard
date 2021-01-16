3c5a62baef19fec89c8df7de6e3ccbaae605d6e2
Mark Delgado <markdelgado94@gmail.com>
2021-01-15 20:54:58 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,48 +1,29 @@
 # Weather-Dashboard
+
+The Weather Dashboard is a webpage that will display daily weather as well as a 5 day forecast. The webpage allows the user to search any city in the United States.The current weather will display an icon of the current weather as well as the temperature, humidity, and wind speed. The 5 day forescast will also present a icon with the temeperature and humidity. When the user inputs another city the previous city will be placed under the search bar. When the user clcks on the previous city it will display the information again. 
  
-# 06 Server-Side APIs: Weather Dashboard
+# How to use:
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+When the webpage is generated the user will see a search bar where they will search the city that they choose.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+The current weather will be displayed along with the 5 day forecast.
 
-## User Story
+When the user searches another city the previous city searched will be saved below the search bar. 
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
 
-## Acceptance Criteria
+![Startpage](./Assets/Weather-page.png)
+![Search Results](Assets/search-results.png)
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-```
 
-The following image demonstrates the application functionality:
+Heres a link to the deployed application:
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
+https://mdelgado1128.github.io/Weather-Dashboard/
 
-## Review
+Here is my Github repository:
 
-You are required to submit the following for review:
+https://github.com/mdelgado1128/Weather-Dashboard
 
-* The URL of the deployed application.
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
 
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
+
--- main.js
+++ main.js
@@ -1,104 +1,110 @@
 
 var weatherfunc = function (cityName) {
 
-   
-
     var APIKey = "297d1db0f7aa491ec1ec6a9e9df90610";
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",&units=imperial&appid=" + APIKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
+    
 
         var iconImg = $("<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png>")
 
-        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-
         $(".header1").html(iconImg);
-        $(".temperature").text("Temperature: " + tempF.toFixed(2));
+        $(".header1").append(response.name);
+        $(".header1").append("(" + new Date(response.dt).toLocaleDateString() + ")" );
+        $(".temperature").text("Temperature: " + response.main.temp);
         $(".humidity").text("Humidity: " + response.main.humidity);
         $(".windspeed").text("Windspeed: " + response.wind.speed + " MPH");
-        // $(".uvIndex").text("UV Index: " + response.main.uv);
-
+       
     });
+
+    var lon =0;
+    var lat =0;
+
+
+    var APIKey = "297d1db0f7aa491ec1ec6a9e9df90610";
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response){
+        
+
+
+    })
+
 
 };
 
 var forecastfunc = function (cityName) {
 
-
-    // var cityName = $(".searchInput").val();
     var APIKey = "297d1db0f7aa491ec1ec6a9e9df90610";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",&units=imperial&appid=" + APIKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
-        console.log(response)
-        // $("forecast").text(response);
-        // $("#dayForecast").text("5 Day Forescast" + response.main.uv);
         var forecast = $("#5forecast")
           forecast.html("")
 
         for (var i = 0; i < response.list.length; i = i + 8) {
 
-           
             var forecastBlock = $("<div>").addClass("header2 col-2 ")
             var eachIcon = $("<h1>").addClass(" header3 eachIcon")
             var eachTemp = $("<div>").addClass("eachTemp")
             var eachHum = $("<div>").addClass("eachHum")
 
-
             var iconImg = $("<img src=http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png>")
-
-            //  .attr("id", i)
-
-
-
            
-            var tempF = (response.list[i].main.temp - 273.15) * 1.80 + 32;
-            // var arr = (response.list;)
+        
             eachIcon.text("(" + new Date(response.list[i].dt_txt).toLocaleDateString() + ")" );
-            eachTemp.text("Temperature:" + tempF.toFixed(2));
+            eachTemp.text("Temperature:" + response.list[i].main.temp);
             eachHum.text("Humidity:" + response.list[i].main.humidity);
-
 
             eachIcon.append(iconImg);
             forecastBlock.append(eachIcon)
             forecastBlock.append(eachTemp)
             forecastBlock.append(eachHum)
             forecast.append(forecastBlock)
-
-
         };
-
-        // $.each()
-
-
-
-
     })
-
-
-
 };
+
+// var uvIndex = function(){
+//     var lon =$(".searchInput").val();
+//     // var lat =$(".searchInput").val();
+
+
+//     var APIKey = "297d1db0f7aa491ec1ec6a9e9df90610";
+//     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lon + "&lon=" + lon + "&appid=" + APIKey;
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response){
+//         console.log(response)
+
+
+
+//     })
+
+
+// };
+
+
 
  var previousSearch = function(cityName){
 var cities =JSON.parse(localStorage.getItem("city")) || [];
-// $(".previousCity").text(searchCity)
 var searchCities = [...new Set(cities)]
 searchCities.push(cityName)
 localStorage.setItem("city", JSON.stringify(searchCities))
-// var citySearch = document.getElementById(".previousCity")
-// previousCity.textcontent = searchCity
 
-//  citySearch.appendChild(searchCity)
 showCities()
  };
 
@@ -113,8 +119,6 @@ showCities()
        })
        loc.append(cityEl)
    });
-
-
  }
 
 $(".weatherBtn").on("click", function (event) {
@@ -124,11 +128,6 @@ $(".weatherBtn").on("click", function (event) {
   previousSearch(cityName)
     weatherfunc(cityName)
     forecastfunc(cityName)
-    
 
-    // localStorage.setItem("city", cityName);
-
-
-    //  previousSearch()
 
 });
